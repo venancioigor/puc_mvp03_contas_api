bd8fd242a1fb3bc05d29b583e7bcb369d551272a
Igor Venancio <igor@investtools.com.br>
2023-08-19 15:59:43 -0300

    Atualizando url da requisição da api bank

--- routes/services/banco_service.py
+++ routes/services/banco_service.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_banco_by_id(id_banco):
-    url = f"http://127.0.0.1:5000/api/bancos/getBancoById?id_banco={id_banco}" 
+    url = f"http://host.docker.internal:5000/api/bancos/getBancoById?id_banco={id_banco}" 
     response = requests.get(url)
 
     if response.status_code == 200:
